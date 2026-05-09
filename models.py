d7064d1d0001364019aae418f5169cb8d1e0fed6
Leonardo Romero <leo.ro151095@gmail.com>
2026-05-09 08:30:41 -0500

    Aumentar tamaño password_hash a 512

--- models.py
+++ models.py
@@ -14,7 +14,7 @@ class User(UserMixin, db.Model):
     celular = db.Column(db.String(40))
     correo_personal = db.Column(db.String(120))
     correo = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
     rol = db.Column(db.String(30), nullable=False)
     area = db.Column(db.String(80))
 
